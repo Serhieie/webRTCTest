320021227bd5e41c7dc69c2a5b2fdbfc383d8e03
Serhiei <Serhieievb@gmail.com>
2024-12-19 18:39:02 +0200

    cleaning + remove https

--- telelegal-back-end/server.js
+++ telelegal-back-end/server.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const https = require('https');
+const http = require('https');
 const express = require('express');
 const socketio = require('socket.io')
 const cors = require('cors')
@@ -9,12 +10,14 @@ app.use(cors())
 app.use(express.static(__dirname + '/public'))
 app.use(express.json())
 
-const key = fs.readFileSync('./certs/cert.key')
-const cert = fs.readFileSync('./certs/cert.crt')
+// const key = fs.readFileSync('./certs/cert.key')
+// const cert = fs.readFileSync('./certs/cert.crt')
 
-const expressServer = https.createServer({ key, cert }, app)
+// const expressServer = https.createServer({ key, cert }, app)
+
+const expressServer = http.createServer({ }, app)
 const io = socketio(expressServer, {
-    cors: ['https://localhost:3000','https://localhost:3001','https://localhost:3002']
+    cors: ['https://localhost:3000', 'https://localhost:3001', 'https://localhost:3002', 'https://www.webrtc-bohd-test.site']
 })
 
 expressServer.listen(9009);
